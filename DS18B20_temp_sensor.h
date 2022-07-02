d7f428c6797bee524ddd8a04840b0c2d8c60da3e
Loader009 <Loader009@users.noreply.github.com>
2022-07-02 21:53:33 +0200

    Create DS18B20_temp_sensor.h

---
+++ DS18B20_temp_sensor.h
@@ -0,0 +1,18 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _DS18B20_temp_sensor_H_
+#define _DS18B20_temp_sensor_H_
+#include "Arduino.h"
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+
+int pow(int x,int n);
+
+#endif /* _DS18B20_temp_sensor_H_ */
